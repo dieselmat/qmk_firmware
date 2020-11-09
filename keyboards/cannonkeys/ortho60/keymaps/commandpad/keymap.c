a5848d0bef31d83031bd22a891784236a571c5f5
Spencer <doyle.mate@gmail.com>
2020-11-09 12:38:43 +0800

    Cannon Keys Command Pad Additional key for Teams

--- keyboards/cannonkeys/ortho60/keymaps/commandpad/keymap.c
+++ keyboards/cannonkeys/ortho60/keymaps/commandpad/keymap.c
@@ -31,14 +31,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,           LGUI(KC_F4),    RGUI(KC_5),     RGUI(KC_4),  RGUI(KC_6),     C(KC_F9),       S(KC_F10),          S(KC_F9),       KC_NO,        LGUI(KC_1),   LGUI(KC_9),        C(KC_F12),
   KC_TAB,           C(KC_F4),       C(S(KC_DOWN)),  RGUI(KC_1),  RGUI(KC_8),     KC_F9,          KC_F8,              LGUI(KC_F8),    C(S(KC_S)),   KC_NO,        C(KC_F),           C(S(KC_F)),
   C(LGUI(KC_TAB)),  S(KC_DEL),      C(KC_SPACE),    RGUI(KC_2),  C(KC_D),        KC_F7,          S(KC_F8),           KC_NO,          LGUI(KC_J),   C(KC_N),      C(S(KC_N)),        C(S(KC_A)),
-  KC_PSCR,          LGUI(KC_PSCR),  C(S(KC_UP)),    KC_NO,       KC_NO,          KC_NO,          KC_NO,              C(KC_K),        C(S(KC_K)),   C(KC_D),      C(LGUI(KC_LEFT)),  C(LGUI(KC_RIGHT)),
-  C(KC_Z),          C(KC_C),        C(KC_V),        C(KC_A),     BL_STEP,        TG(_WIN),          KC_ENTER,           RESET,          C(KC_B),      LGUI(KC_F7),  LGUI(KC_LEFT),     LGUI(KC_RIGHT)
+  KC_PSCR,          LGUI(KC_PSCR),  C(S(KC_UP)),    RGUI(KC_3),  KC_NO,          KC_NO,          KC_NO,              C(KC_K),        C(S(KC_K)),   C(KC_D),      C(LGUI(KC_LEFT)),  C(LGUI(KC_RIGHT)),
+  C(KC_Z),          C(KC_C),        C(KC_V),        C(KC_A),     BL_STEP,        TG(_WIN),       KC_ENTER,           RESET,          C(KC_B),      LGUI(KC_F7),  LGUI(KC_LEFT),     LGUI(KC_RIGHT)
 ),
 [_WIN] = LAYOUT_ortho_5x12(
   KC_ESC,           A(KC_F4),       RGUI(KC_5),     RGUI(KC_4),  RGUI(KC_6),     C(KC_F9),       S(KC_F10),          S(KC_F9),       KC_NO,        A(KC_1),      A(KC_9),           C(KC_F12),
   KC_TAB,           C(KC_F4),       C(S(KC_DOWN)),  RGUI(KC_1),  RGUI(KC_8),     KC_F9,          KC_F8,              A(KC_F8),       C(S(KC_S)),   KC_NO,        C(KC_F),           C(S(KC_F)),
   C(A(KC_TAB)),     S(KC_DEL),      C(KC_SPACE),    RGUI(KC_2),  C(KC_D),        KC_F7,          S(KC_F8),           KC_NO,          A(KC_J),      C(KC_N),      C(S(KC_N)),        C(S(KC_A)),
-  KC_PSCR,          A(KC_PSCR),     C(S(KC_UP)),    KC_NO,       KC_NO,          KC_NO,          KC_NO,              C(KC_K),        C(S(KC_K)),   C(KC_D),      C(A(KC_LEFT)),     C(A(KC_RIGHT)),
+  KC_PSCR,          A(KC_PSCR),     C(S(KC_UP)),    RGUI(KC_3),  KC_NO,          KC_NO,          KC_NO,              C(KC_K),        C(S(KC_K)),   C(KC_D),      C(A(KC_LEFT)),     C(A(KC_RIGHT)),
   C(KC_Z),          C(KC_C),        C(KC_V),        C(KC_A),     BL_STEP,        KC_TRANSPARENT, KC_ENTER,           RESET,          C(KC_B),      A(KC_F7),     A(KC_LEFT),        A(KC_RIGHT)
 )
 };
